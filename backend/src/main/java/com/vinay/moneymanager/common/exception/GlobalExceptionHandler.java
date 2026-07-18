90b46a16103129036d409aa804eef69bde45ab07
Vinay R <vinayroffl@gmail.com>
2026-07-19 01:20:22 +0530

    feat: added FeatureNotImplementedException

--- backend/src/main/java/com/vinay/moneymanager/common/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/vinay/moneymanager/common/exception/GlobalExceptionHandler.java
@@ -2,57 +2,71 @@ package com.vinay.moneymanager.common.exception;
 
 import com.vinay.moneymanager.common.response.ApiError;
 import com.vinay.moneymanager.common.response.ValidationError;
+import java.time.LocalDateTime;
+import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    // ResourceNotFoundException
-    @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ApiError> handleException(ResourceNotFoundException exception) {
-        ApiError apiError = ApiError.builder().message(exception.getMessage()).timestamp(LocalDateTime.now()).build();
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(apiError);
-    }
+  // ResourceNotFoundException
+  @ExceptionHandler(ResourceNotFoundException.class)
+  public ResponseEntity<ApiError> handleException(ResourceNotFoundException exception) {
+    ApiError apiError =
+        ApiError.builder().message(exception.getMessage()).timestamp(LocalDateTime.now()).build();
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(apiError);
+  }
 
-    // DuplicateResourceException
-    @ExceptionHandler(DuplicateResourceException.class)
-    public ResponseEntity<ApiError> handleException(DuplicateResourceException exception) {
-        ApiError apiError = ApiError.builder().message(exception.getMessage()).timestamp(LocalDateTime.now()).build();
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(apiError);
-    }
+  // DuplicateResourceException
+  @ExceptionHandler(DuplicateResourceException.class)
+  public ResponseEntity<ApiError> handleException(DuplicateResourceException exception) {
+    ApiError apiError =
+        ApiError.builder().message(exception.getMessage()).timestamp(LocalDateTime.now()).build();
+    return ResponseEntity.status(HttpStatus.CONFLICT).body(apiError);
+  }
 
-    // InvalidCredentialsException
-    @ExceptionHandler(InvalidCredentialsException.class)
-    public ResponseEntity<ApiError> handleInvalidCredentialsException(InvalidCredentialsException exception) {
-        ApiError apiError = ApiError.builder().message(exception.getMessage()).timestamp(LocalDateTime.now()).build();
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(apiError);
-    }
+  // InvalidCredentialsException
+  @ExceptionHandler(InvalidCredentialsException.class)
+  public ResponseEntity<ApiError> handleInvalidCredentialsException(
+      InvalidCredentialsException exception) {
+    ApiError apiError =
+        ApiError.builder().message(exception.getMessage()).timestamp(LocalDateTime.now()).build();
+    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(apiError);
+  }
 
-    // MethodArgumentNotValidException
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ApiError> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+  // MethodArgumentNotValidException
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ApiError> handleMethodArgumentNotValidException(
+      MethodArgumentNotValidException exception) {
 
-        List<ValidationError> validationErrors = exception.getBindingResult().getFieldErrors().stream()
-                .map(fieldError ->
-                        ValidationError.builder()
-                                .field(fieldError.getField())
-                                .message(fieldError.getDefaultMessage())
-                                .build())
-                .toList();
-        ApiError apiError = ApiError.builder()
-                .message("Validation failed")
-                .timestamp(LocalDateTime.now())
-                .errors(validationErrors)
-                .build();
+    List<ValidationError> validationErrors =
+        exception.getBindingResult().getFieldErrors().stream()
+            .map(
+                fieldError ->
+                    ValidationError.builder()
+                        .field(fieldError.getField())
+                        .message(fieldError.getDefaultMessage())
+                        .build())
+            .toList();
+    ApiError apiError =
+        ApiError.builder()
+            .message("Validation failed")
+            .timestamp(LocalDateTime.now())
+            .errors(validationErrors)
+            .build();
 
-        return ResponseEntity.badRequest().body(apiError);
-    }
+    return ResponseEntity.badRequest().body(apiError);
+  }
 
+  @ExceptionHandler(FeatureNotImplementedException.class)
+  public ResponseEntity<ApiError> handleFeatureNotImplementedException(
+      FeatureNotImplementedException exception) {
+    ApiError apiError =
+        ApiError.builder().message(exception.getMessage()).timestamp(LocalDateTime.now()).build();
+    return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(apiError);
+  }
 }
