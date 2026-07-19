5b2762ac50edcb05f5ff24f913be2755c5a1bac1
Vinay R <vinayroffl@gmail.com>
2026-07-19 22:49:15 +0530

    feat(budget): added Budget repository

---
+++ backend/src/main/java/com/vinay/moneymanager/budget/BudgetRepository.java
@@ -0,0 +1,22 @@
+package com.vinay.moneymanager.budget;
+
+import com.vinay.moneymanager.budget.entity.Budget;
+import com.vinay.moneymanager.transaction.entity.Category;
+import com.vinay.moneymanager.user.entity.User;
+import java.time.Month;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BudgetRepository extends JpaRepository<Budget, UUID> {
+
+  Optional<Budget> findByUserAndCategoryAndMonthAndYear(
+      User user, Category category, Month month, int year);
+
+  List<Budget> findByUser(User user);
+
+  List<Budget> findByUserAndMonthAndYear(User user, Month month, int year);
+}
